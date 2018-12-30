e4fb5a6fae08fc93ccd0b3e0efcf435856a96f0b
BenjaVR <benjamin.v.renterghem@gmail.com>
2018-12-30 22:54:43 +0100

    Removed function that is not being used anymore.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,3 @@
 import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
-import { UserRecord } from "firebase-functions/lib/providers/auth";
 
 admin.initializeApp();
-const auth = admin.auth();
-
-/**
- * Disables users after they sign up. This prevents random people (somehow succeeded to sign up) from logging in.
- */
-export const disableUserAfterSignUp = functions.auth.user().onCreate((user: UserRecord): void => {
-    auth.updateUser(user.uid, {
-        disabled: true,
-    });
-});
