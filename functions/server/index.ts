b6c75163cf1f322693657e6e2b3383c2b23df33e
BenjaVR <benjamin.v.renterghem@gmail.com>
2019-01-21 20:04:18 +0100

    Delete educationId field in Student when Education was deleted

--- functions/server/index.ts
+++ functions/server/index.ts
@@ -10,8 +10,8 @@ firestore.settings({
     timestampsInSnapshots: true,
 });
 
-exports.onEducationDelete = functions.firestore.document("/educations/{id}").onDelete((snapShot) => {
-    const educationId = snapShot.id;
+exports.onEducationDelete = functions.firestore.document("/educations/{id}").onDelete((snapshot) => {
+    const educationId = snapshot.id;
 
     // Delete relations on departments
     const departmentsPromise = firestore.collection("/departments")
@@ -46,21 +46,46 @@ exports.onEducationDelete = functions.firestore.document("/educations/{id}").onD
     return Promise.all([departmentsPromise]);
 });
 
-exports.onSchoolDelete = functions.firestore.document("/schools/{id}").onDelete((snapShot) => {
-    const schoolId = snapShot.id;
+exports.onSchoolDelete = functions.firestore.document("/schools/{id}").onDelete((snapshot) => {
+    const schoolId = snapshot.id;
 
     // Delete relations on students
+    const schoolIdField: keyof IStudent = "schoolId";
     const studentsPromise = firestore.collection("/students")
-        .where("schoolId", "==", schoolId)
+        .where(schoolIdField, "==", schoolId)
         .get()
         .then((querySnapshot) => {
             const innerPromises = [];
 
             const mappedStudents = ModelMapper.mapDocsToObjects<IStudent>(querySnapshot.docs);
             mappedStudents.forEach((student) => {
-                const schoolIdField: keyof IStudent = "schoolId";
                 const updatedStudent = {
                     [schoolIdField]: admin.firestore.FieldValue.delete(),
+                };
+                innerPromises.push(firestore.doc(`/students/${student.id}`).update(updatedStudent));
+            });
+
+            return Promise.all([innerPromises]);
+        });
+
+    return Promise.all([studentsPromise]);
+});
+
+exports.onEducationDelete = functions.firestore.document("/educations/{id}").onDelete((snapshot) => {
+    const educationId = snapshot.id;
+
+    // Delete relations on students
+    const educationIdField: keyof IStudent = "educationId";
+    const studentsPromise = firestore.collection("/students")
+        .where(educationIdField, "==", educationId)
+        .get()
+        .then((querySnapshot) => {
+            const innerPromises = [];
+
+            const mappedStudents = ModelMapper.mapDocsToObjects<IStudent>(querySnapshot.docs);
+            mappedStudents.forEach((student) => {
+                const updatedStudent = {
+                    [educationIdField]: admin.firestore.FieldValue.delete(),
                 };
                 innerPromises.push(firestore.doc(`/students/${student.id}`).update(updatedStudent));
             });
