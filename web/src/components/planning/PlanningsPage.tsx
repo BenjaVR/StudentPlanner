729e376de157b54f8bb6738b1363ad60791bab46
BenjaVR <benjamin.v.renterghem@gmail.com>
2019-01-27 22:32:32 +0100

    Added responsive next/prev buttons on the calendar component

--- web/src/components/planning/PlanningsPage.tsx
+++ web/src/components/planning/PlanningsPage.tsx
@@ -1,4 +1,4 @@
-import { Badge, Button, Calendar, Col, List, notification, Row, Tag, Tooltip } from "antd";
+import { Button, Calendar, Col, List, notification, Row, Spin, Tag, Tooltip } from "antd";
 import React from "react";
 import { IStudent } from "studentplanner-functions/shared/contract/IStudent";
 import { AnyRouteComponentProps } from "../../routes";
@@ -11,9 +11,12 @@ interface IPlanningsPageState {
     unplannedStudents: IStudent[];
     areStudentsLoading: boolean;
     showOnlyConfirmedStudents: boolean;
+    isCalendarLoading: boolean;
 }
 
 class PlanningsPage extends React.Component<PlanningsPageProps, IPlanningsPageState> {
+
+    private calendarRef: Calendar | null = null;
 
     private readonly studentsService = new StudentsService();
     private readonly studentLoadFailedNotificationKey = "studentLoadFailed";
@@ -25,11 +28,14 @@ class PlanningsPage extends React.Component<PlanningsPageProps, IPlanningsPageSt
             unplannedStudents: [],
             areStudentsLoading: false,
             showOnlyConfirmedStudents: true,
+            isCalendarLoading: false,
         };
 
         this.renderStudentListHeader = this.renderStudentListHeader.bind(this);
         this.renderStudentListItem = this.renderStudentListItem.bind(this);
         this.handleReloadStudents = this.handleReloadStudents.bind(this);
+        this.handleNextMonth = this.handleNextMonth.bind(this);
+        this.handlePrevMonth = this.handlePrevMonth.bind(this);
     }
 
     public componentWillMount(): void {
@@ -40,7 +46,13 @@ class PlanningsPage extends React.Component<PlanningsPageProps, IPlanningsPageSt
         return (
             <Row type="flex">
                 <Col span={24} xl={16}>
-                    <Calendar mode="month" />
+                    <Spin spinning={this.state.isCalendarLoading}>
+                        <div className={styles.buttonsInCalendarComponent}>
+                            <Button icon="left" onClick={this.handlePrevMonth} />
+                            <Button icon="right" onClick={this.handleNextMonth} />
+                        </div>
+                        <Calendar mode="month" ref={(ref) => this.calendarRef = ref} />
+                    </Spin>
                 </Col>
                 <Col span={24} xl={8}>
                     <List
@@ -94,6 +106,29 @@ class PlanningsPage extends React.Component<PlanningsPageProps, IPlanningsPageSt
 
     private handlePlanStudent(student: IStudent): void {
         alert(student.firstName);
+    }
+
+    private handleNextMonth(): void {
+        this.doMonthChange("next");
+    }
+
+    private handlePrevMonth(): void {
+        this.doMonthChange("prev");
+    }
+
+    private doMonthChange(action: "next" | "prev"): void {
+        if (this.calendarRef === null) {
+            return;
+        }
+
+        const currentMoment = this.calendarRef.state.value;
+
+        if (action === "next") {
+            this.calendarRef.setValue(currentMoment.add(1, "month"), "changePanel");
+        }
+        if (action === "prev") {
+            this.calendarRef.setValue(currentMoment.subtract(1, "month"), "changePanel");
+        }
     }
 
     private loadStudents(): void {
