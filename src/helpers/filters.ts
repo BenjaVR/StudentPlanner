a87a4785f5a186a40556dfcf7c77d6e549e0bcd2
Benjamin Van Renterghem <benjamin.v.renterghem@gmail.com>
2019-11-21 20:44:54 +0100

    Prettier

--- src/helpers/filters.ts
+++ src/helpers/filters.ts
@@ -17,28 +17,48 @@ export function hasElementWithId<T extends IFirestoreEntityBase>(elements: T[], 
 
 export function studentsPlannedInDay(students: Student[], date: moment.Moment): Student[] {
     return students.filter((student) => {
-        return student.internship !== undefined
-            && date.startOf("day").isBetween(
-                student.internship.startDate.startOf("day"),
-                student.internship.endDate.startOf("day"),
-                "day",
-                "[]",
-            );
+        return (
+            student.internship !== undefined &&
+            date
+                .startOf("day")
+                .isBetween(
+                    student.internship.startDate.startOf("day"),
+                    student.internship.endDate.startOf("day"),
+                    "day",
+                    "[]"
+                )
+        );
     });
 }
 
-export function studentsPlannedFullyInRange(students: Student[], from: moment.Moment | undefined, until: moment.Moment | undefined): Student[] {
+export function studentsPlannedFullyInRange(
+    students: Student[],
+    from: moment.Moment | undefined,
+    until: moment.Moment | undefined
+): Student[] {
     return students.filter((student) => {
-        return from !== undefined && until !== undefined && student.internship !== undefined
-            && student.internship.startDate.isSameOrAfter(from)
-            && student.internship.endDate.isSameOrBefore(until);
+        return (
+            from !== undefined &&
+            until !== undefined &&
+            student.internship !== undefined &&
+            student.internship.startDate.isSameOrAfter(from) &&
+            student.internship.endDate.isSameOrBefore(until)
+        );
     });
 }
 
-export function studentsPlannedPartiallyInRange(students: Student[], from: moment.Moment | undefined, until: moment.Moment | undefined): Student[] {
+export function studentsPlannedPartiallyInRange(
+    students: Student[],
+    from: moment.Moment | undefined,
+    until: moment.Moment | undefined
+): Student[] {
     return students.filter((student) => {
-        return from !== undefined && until !== undefined && student.internship !== undefined
-            && ((student.internship.startDate.isBefore(from) && student.internship.endDate.isSameOrAfter(from))
-                || (student.internship.endDate.isAfter(until) && student.internship.startDate.isSameOrBefore(until)));
+        return (
+            from !== undefined &&
+            until !== undefined &&
+            student.internship !== undefined &&
+            ((student.internship.startDate.isBefore(from) && student.internship.endDate.isSameOrAfter(from)) ||
+                (student.internship.endDate.isAfter(until) && student.internship.startDate.isSameOrBefore(until)))
+        );
     });
 }
